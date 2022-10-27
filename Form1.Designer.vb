8d94beb1ba476a60a2c685c794aa9b0191f2f12d
MarkHenderson02 <114543984+MarkHenderson02@users.noreply.github.com>
2022-10-27 10:43:35 +0800

    Array2Dimension

---
+++ Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cbProdi = New System.Windows.Forms.ComboBox()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtNim = New System.Windows.Forms.TextBox()
+        Me.btnProses = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LV1 = New System.Windows.Forms.ListView()
+        Me.cbKelamin = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cbProdi
+        '
+        Me.cbProdi.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbProdi.FormattingEnabled = True
+        Me.cbProdi.Location = New System.Drawing.Point(223, 161)
+        Me.cbProdi.Name = "cbProdi"
+        Me.cbProdi.Size = New System.Drawing.Size(162, 24)
+        Me.cbProdi.TabIndex = 15
+        '
+        'txtNama
+        '
+        Me.txtNama.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNama.Location = New System.Drawing.Point(223, 82)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(197, 22)
+        Me.txtNama.TabIndex = 14
+        '
+        'txtNim
+        '
+        Me.txtNim.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNim.Location = New System.Drawing.Point(223, 41)
+        Me.txtNim.Name = "txtNim"
+        Me.txtNim.Size = New System.Drawing.Size(109, 22)
+        Me.txtNim.TabIndex = 13
+        '
+        'btnProses
+        '
+        Me.btnProses.Location = New System.Drawing.Point(223, 216)
+        Me.btnProses.Name = "btnProses"
+        Me.btnProses.Size = New System.Drawing.Size(98, 35)
+        Me.btnProses.TabIndex = 12
+        Me.btnProses.Text = "Proses"
+        Me.btnProses.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(126, 164)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 16)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Prodi"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(126, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 16)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Nama "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(126, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 16)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "NIM"
+        '
+        'LV1
+        '
+        Me.LV1.HideSelection = False
+        Me.LV1.Location = New System.Drawing.Point(12, 270)
+        Me.LV1.Name = "LV1"
+        Me.LV1.Size = New System.Drawing.Size(522, 256)
+        Me.LV1.TabIndex = 8
+        Me.LV1.UseCompatibleStateImageBehavior = False
+        '
+        'cbKelamin
+        '
+        Me.cbKelamin.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbKelamin.FormattingEnabled = True
+        Me.cbKelamin.Location = New System.Drawing.Point(223, 121)
+        Me.cbKelamin.Name = "cbKelamin"
+        Me.cbKelamin.Size = New System.Drawing.Size(162, 24)
+        Me.cbKelamin.TabIndex = 17
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(126, 124)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(91, 16)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Jenis Kelamin"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(546, 538)
+        Me.Controls.Add(Me.cbKelamin)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cbProdi)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.txtNim)
+        Me.Controls.Add(Me.btnProses)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LV1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "2 Dimension"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cbProdi As ComboBox
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents txtNim As TextBox
+    Friend WithEvents btnProses As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LV1 As ListView
+    Friend WithEvents cbKelamin As ComboBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Arr(3, 1) As String
+        Arr(0, 0) = "NIM"
+        Arr(0, 1) = "Nama"
+        Arr(1, 0) = "Jenis Kelamin"
+        Arr(1, 1) = "Prodi"
+        Arr(2, 0) = "Laki-laki"
+        Arr(2, 1) = "Perempuan"
+        Arr(3, 0) = "Tehnik Informatika"
+        Arr(3, 1) = "Sistem Informasi"
+
+        LV1.GridLines = True
+        LV1.View = View.Details
+
+        For Baris = 0 To 1
+            For Kolom = 0 To 1
+                LV1.Columns.Add(Arr(Baris, Kolom), 100)
+            Next Kolom
+        Next Baris
+
+        For Baris = 2 To 2
+            For Kolom = 0 To 1
+                cbKelamin.Items.Add(Arr(Baris, Kolom))
+            Next Kolom
+        Next Baris
+
+        For Baris = 3 To 3
+            For Kolom = 0 To 1
+                cbProdi.Items.Add(Arr(Baris, Kolom))
+            Next Kolom
+        Next Baris
+    End Sub
+
+    Private Sub btnProses_Click(sender As Object, e As EventArgs) Handles btnProses.Click
+        Dim Arr(3) As String
+        Arr(0) = txtNim.Text
+        Arr(1) = txtNama.Text
+        Arr(2) = cbKelamin.Text
+        Arr(3) = cbProdi.Text
+
+        Dim listitem As ListViewItem
+        listitem = New ListViewItem
+
+        If txtNama.Text <> "" And txtNim.Text <> "" Then
+            Arr(0) = txtNim.Text
+            Dim ketemu = False
+            For i = 0 To LV1.Items.Count - 1
+                If LV1.Items(i).Text = txtNim.Text Then
+                    ketemu = True
+                    Exit For
+                End If
+            Next
+            If Not ketemu Then
+                listitem = LV1.Items.Add(Arr(0))
+                listitem.SubItems.Add(Arr(1))
+                listitem.SubItems.Add(Arr(2))
+                listitem.SubItems.Add(Arr(3))
+
+                txtNim.Text = txtNim.Text + 1
+            Else
+                MsgBox("Nomor Induk Mahasiswa Sudah ada ")
+            End If
+        Else
+            MsgBox("Data Nomor Induk Mahasiswa (NIM) tidak boleh kosong")
+        End If
+        txtNama.Text = ""
+            cbKelamin.Text = ""
+            cbProdi.Text = ""
+        txtNama.Focus()
+    End Sub
+End Class
